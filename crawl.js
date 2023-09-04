c268414ca5f60189726cf27b9e63003fe9707f1a
arjun girish <arjungirish2009@gmail.com>
2023-09-04 14:36:53 +0200

    getUrlfromHtml,handles relative and absolte url

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const {JSDOM}=require("jsdom");
+
+function getUrlFromHtml(html,baseUrl){
+  let linkArray=[];
+  const dom=new JSDOM(html); //converting string to DOM
+  const anchorElement=dom.window.document.querySelectorAll('a');
+  for(ele of anchorElement){
+    if(ele.href.slice(0,1)==='/'){ //relative url
+
+      try{
+        const urlObj=new URL(`${baseUrl}${ele.href}`);
+        linkArray.push(urlObj.href)
+      }catch(error){
+        console.log(`relative url: ${error.message}`);
+      }
+    }
+    else{
+      try{
+        const urlObj=new URL(ele.href)
+        linkArray.push(ele.href)
+      }catch(error){
+        console.log(`absolute url: ${error.message}`)
+      }
+    }
+  }
+  return linkArray;
+}
+
 function normaliseURL(url){
   const urlObj=new URL(url);
   const host= `${urlObj.hostname}${urlObj.pathname}`;
@@ -8,5 +36,5 @@ function normaliseURL(url){
 }
 
 module.exports={
-  normaliseURL
+  normaliseURL,getUrlFromHtml
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,10 +1,29 @@
-const {normaliseURL}=require("./crawl.js");
+const {normaliseURL,getUrlFromHtml}=require("./crawl.js");
 const {test,expect}=require("@jest/globals");
 
 test('normaliseURL',()=>{
-  const input="https://arjun.com/resource/";
+  const input="https://ARJUN.com/resource/";
   const actual =normaliseURL(input);
   const expected="arjun.com/resource";
+
+  expect(actual).toEqual(expected);
+})
+
+test('getUrlFromHtml',()=>{
+  const input=`
+  <html>
+    <body>
+      <a href="https://arjun.com/resource"
+          resource link
+      </a>
+      <a href="invalid">
+      Link
+      </a>
+    </body>
+  </html>  `;
+  const baseUrl="https://arjun.com"
+  const actual =getUrlFromHtml(input,baseUrl);
+  const expected=["https://arjun.com/resource"];
 
   expect(actual).toEqual(expected);
 })
